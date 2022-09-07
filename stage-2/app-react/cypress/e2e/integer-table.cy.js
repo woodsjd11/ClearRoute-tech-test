ce8fe5075bd62fc38b69fd3768fd1f37c5ab0006
woodsjd11 <104032001+woodsjd11@users.noreply.github.com>
2022-09-07 11:35:50 +0100

    add test for displaying one table

--- stage-2/app-react/cypress/e2e/integer-table.cy.js
+++ stage-2/app-react/cypress/e2e/integer-table.cy.js
@@ -2,9 +2,18 @@ describe("start the test button", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
   });
-  it('navigates to the test page', () => {
-    cy.get('[href="/test"] button').click()
-    cy.url().should('eq', 'http://localhost:3000/test')
+  it("navigates to the test page", () => {
+    cy.get('[href="/test"] button').click();
+    cy.url().should("eq", "http://localhost:3000/test");
   });
 });
 
+describe("integer table", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[href="/test"] button').click();
+  });
+  it("displays one table by default ", () => {
+    cy.get("table").should("have.length", 1);
+  });
+});
