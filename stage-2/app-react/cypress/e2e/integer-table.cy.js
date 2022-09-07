01b762215942136f51bc73c2298253e032300b3b
woodsjd11 <104032001+woodsjd11@users.noreply.github.com>
2022-09-07 11:55:45 +0100

    add tests for confirming row numbers

--- stage-2/app-react/cypress/e2e/integer-table.cy.js
+++ stage-2/app-react/cypress/e2e/integer-table.cy.js
@@ -19,4 +19,46 @@ describe("integer table", () => {
   it("should have 4 rows", () => {
     cy.get("tr").should("have.length", 4);
   });
+  it("should have the expected numbers in each row", () => {
+    cy.get('[testid="row-0"]')
+      .find("td")
+      .then((data) => {
+        const cells = [...data];
+        const rowContent = [];
+        cells.forEach((cell) => {
+          rowContent.push(+cell.innerText);
+        });
+        expect(rowContent).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      });
+      cy.get('[testid="row-1"]')
+      .find("td")
+      .then((data) => {
+        const cells = [...data];
+        const rowContent = [];
+        cells.forEach((cell) => {
+          rowContent.push(+cell.innerText);
+        });
+        expect(rowContent).to.deep.equal([23, 50, 63, 90, 10, 30, 155, 23, 18]);
+      });
+      cy.get('[testid="row-2"]')
+      .find("td")
+      .then((data) => {
+        const cells = [...data];
+        const rowContent = [];
+        cells.forEach((cell) => {
+          rowContent.push(+cell.innerText);
+        });
+        expect(rowContent).to.deep.equal([133, 60, 23,92,6,7,168,16,19]);
+      });
+      cy.get('[testid="row-3"]')
+      .find("td")
+      .then((data) => {
+        const cells = [...data];
+        const rowContent = [];
+        cells.forEach((cell) => {
+          rowContent.push(+cell.innerText);
+        });
+        expect(rowContent).to.deep.equal([30,43,29,10,50,40,99,51,12]);
+      });
+  });
 });
