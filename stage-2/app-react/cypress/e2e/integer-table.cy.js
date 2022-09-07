9310d99ad2d9797f6c677b8b3f3cc84e2742159f
woodsjd11 <104032001+woodsjd11@users.noreply.github.com>
2022-09-07 12:31:08 +0100

    initial function passes example provided where null is not returned

--- stage-2/app-react/cypress/e2e/integer-table.cy.js
+++ stage-2/app-react/cypress/e2e/integer-table.cy.js
@@ -1,3 +1,5 @@
+import findIndex from "../../challenge-function.js";
+
 describe("start the test button", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
@@ -30,7 +32,7 @@ describe("integer table", () => {
         });
         expect(rowContent).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9]);
       });
-      cy.get('[testid="row-1"]')
+    cy.get('[testid="row-1"]')
       .find("td")
       .then((data) => {
         const cells = [...data];
@@ -40,7 +42,7 @@ describe("integer table", () => {
         });
         expect(rowContent).to.deep.equal([23, 50, 63, 90, 10, 30, 155, 23, 18]);
       });
-      cy.get('[testid="row-2"]')
+    cy.get('[testid="row-2"]')
       .find("td")
       .then((data) => {
         const cells = [...data];
@@ -48,9 +50,9 @@ describe("integer table", () => {
         cells.forEach((cell) => {
           rowContent.push(+cell.innerText);
         });
-        expect(rowContent).to.deep.equal([133, 60, 23,92,6,7,168,16,19]);
+        expect(rowContent).to.deep.equal([133, 60, 23, 92, 6, 7, 168, 16, 19]);
       });
-      cy.get('[testid="row-3"]')
+    cy.get('[testid="row-3"]')
       .find("td")
       .then((data) => {
         const cells = [...data];
@@ -58,7 +60,16 @@ describe("integer table", () => {
         cells.forEach((cell) => {
           rowContent.push(+cell.innerText);
         });
-        expect(rowContent).to.deep.equal([30,43,29,10,50,40,99,51,12]);
+        expect(rowContent).to.deep.equal([30, 43, 29, 10, 50, 40, 99, 51, 12]);
       });
+  });
+});
+
+describe("findInteger function", () => {
+  it("returns the correct index for a known example", () => {
+    const numbers = [10, 15, 5, 7, 1, 24, 36, 2];
+    const result = findIndex(numbers);
+    expect(result).to.equal(5);
+    
   });
 });
